4bf3c649c780d0440c90c5b8dcf25f1ebf07cd62
nohohon1820 <ishibashix@gmail.com>
2023-08-05 19:21:35 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,15 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  
   belongs_to :user
-  has_one :order
+  has_one    :order
 
   has_one_attached :image
+
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :charged
+  belongs_to :prefecture
+  belongs_to :shipping_day
 
 end
