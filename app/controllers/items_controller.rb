fa23de03e7488e8f204359b5899bf904b1aa09c6
nohohon1820 <ishibashix@gmail.com>
2023-08-05 12:17:25 +0900

    DBへ値保存のためのitemのcreateアクションの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,20 @@ class ItemsController < ApplicationController
   def index
     @item = Item.includes(:user).order('created_at DESC')
   end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :introduction, :category_id, :condition_id, :charged_id, :prefecture_id, :shipping_day_id, :price).merge(user_id: current_user.id))
+  end
+  
 end
