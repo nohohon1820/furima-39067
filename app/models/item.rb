ecb1798f30a60e4fc5943875bcd008d03f0b0b18
nohohon1820 <ishibashix@gmail.com>
2023-08-06 11:26:06 +0900

    itemモデル　バリデーションの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,4 +12,25 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :shipping_day
 
+  with_options presence: true do
+    validates :user_id
+    validates :image
+    validates :name
+    validates :introduction
+    validates :category_id
+    validates :condition_id
+    validates :charged_id
+    validates :prefecture_id
+    validates :shipping_day_id
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  end
+
+  with_options numericality: { other_than: 0 } do
+    validates :category_id
+    validates :condition_id
+    validates :charged_id
+    validates :prefecture_id
+    validates :shipping_day_id
+  end
+
 end
