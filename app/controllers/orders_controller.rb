d428a5efb9af799fdbfd803df72b2f13ee90bae7
nohohon1820 <ishibashix@gmail.com>
2023-08-12 10:10:01 +0900

    値（フォームの情報）をDBへ保存する実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,10 +5,23 @@ class OrdersController < ApplicationController
     @order_form = OrderForm.new
   end
 
+  def create
+    @order_form = OrderForm.new(order_params)
+    if @order_form.valid?
+      pay_item
+      @order_form.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_form).permit(:post_code, :prefecture_id, :city, :address, :building_name, :phone_number).merge(usesr_id: current_user_id, item_id: params[:item_id], token: params[:token])
+  end
 
 
 
-
-
-  
 end
