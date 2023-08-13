526d3e62ce3fc1645d6be182b6539e91271ef150
nohohon1820 <ishibashix@gmail.com>
2023-08-14 08:42:27 +0900

    orders controllerの修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -25,7 +25,9 @@ class OrdersController < ApplicationController
 
   def not_purchased_item
     @item = Item.find(params[:item_id])
-    redirect_to root_path if current_user.id == @item.user_id || @item.order.present?
+    if current_user.id == @item.user_id || @item.order.present?
+     redirect_to root_path 
+    end
   end
 
   def pay_item
