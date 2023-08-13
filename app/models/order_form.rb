dd5d789b1b8b83a5712602ba4bb3075d35d82f66
nohohon1820 <ishibashix@gmail.com>
2023-08-13 18:54:43 +0900

    orderコントローラー修正

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -15,6 +15,6 @@ class OrderForm
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
-    Payment.create(order_id: order.id, post_code: post_code, prefecture_id: prefecture_id, city: city, address: address, building_name: building_name, phone_number: phone_number)
+    Destination.create(order_id: order.id, post_code: post_code, prefecture_id: prefecture_id, city: city, address: address, building_name: building_name, phone_number: phone_number)
   end
 end
